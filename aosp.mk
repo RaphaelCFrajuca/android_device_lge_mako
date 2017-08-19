2747fe24d2b90a21b23096c7665b2da0c5c559db
BadGuy <badguy5525@gmail.com>
2017-08-18 22:05:08 -0300

    Init Tree ViperOS

---
+++ aosp.mk
@@ -0,0 +1,16 @@
+# Release name
+PRODUCT_RELEASE_NAME := mako
+
+# Inherit some common VIPER stuff.
+$(call inherit-product, vendor/viper/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/lge/mako/aosp_mako.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := mako
+PRODUCT_NAME := viper_mako
+PRODUCT_BRAND := lge
+PRODUCT_MANUFACTURER := lge
+PRODUCT_MODEL := Nexus 4
+PRODUCT_CHARACTERISTICS := phone
---
+++ viper_mako.mk
@@ -0,0 +1,30 @@
+#
+# Copyright 2013 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+$(call inherit-product, device/lge/mako/full_mako.mk)
+
+PRODUCT_NAME := viper_mako
+PRODUCT_MODEL := Nexus 4
+PRODUCT_CHARACTERISTICS := phone
+PRODUCT_BRAND := Android
+PRODUCT_DEVICE := mako
+PRODUCT_MANUFACTURER := LGE
+PRODUCT_RESTRICT_VENDOR_FILES := false
+
+$(call inherit-product, vendor/viper/config/common_full_phone.mk)
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 768
